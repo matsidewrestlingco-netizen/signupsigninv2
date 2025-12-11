0e153094e4e083e4f7ea2ece312a56fbe62ca22f
matsidewrestlingco-netizen <matsidewrestlingco@gmail.com>
2025-12-11 14:56:59 -0500

    Update navbar.js

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -1,9 +1,20 @@
 export function loadNavbar() {
+  console.log("loadNavbar() called");
+
   fetch("./components/navbar.html")
-    .then(res => res.text())
+    .then(res => {
+      console.log("fetch response:", res.status);
+      return res.text();
+    })
     .then(html => {
+      console.log("navbar HTML loaded:", html);
       const target = document.getElementById("navbar");
-      if (target) target.innerHTML = html;
+      if (target) {
+        target.innerHTML = html;
+        console.log("navbar injected successfully");
+      } else {
+        console.log("ERROR: #navbar not found");
+      }
     })
     .catch(err => console.error("Navbar failed to load:", err));
 }
