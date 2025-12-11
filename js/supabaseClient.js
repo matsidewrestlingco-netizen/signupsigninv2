f790970eec97fb57d86c535595cab79ae22686fd
matsidewrestlingco-netizen <matsidewrestlingco@gmail.com>
2025-12-11 15:08:21 -0500

    Update supabaseClient.js

---
+++ js/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://yonrgtqanhoeriictnyw.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlvbnJndHFhbmhvZXJpaWN0bnl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyMTgyNzAsImV4cCI6MjA4MDc5NDI3MH0.CGTxKmy-J2gL2aZy9sIuETmkf8cmB4Dgm9IA_H6UgF0"
+);
